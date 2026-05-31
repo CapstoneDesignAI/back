78911f036b795471b3931a696e880436af41aef4
ddurin <surin.kim0714@gmail.com>
2026-06-01 01:07:20 +0900

    Fix : 유저 인증(Depends) 누락 수정

--- app/api/v1/endpoints/emblems.py
+++ app/api/v1/endpoints/emblems.py
@@ -7,7 +7,7 @@ router = APIRouter(prefix="/emblems", tags=["emblems"])
 
 @router.get("", response_model=list[EmblemItem], summary="내 엠블럼 보관함 조회")
 def read_emblems(
-    user_id: str = "5fd0d467-03cb-4364-b8da-bb1d94d32f35"
+    user_id: str = Depends(get_current_user)
 ):
     result = get_user_emblems(user_id)
     return result
